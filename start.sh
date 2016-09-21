cc069cdb8a56bc990a7a5711839974c46b1b9fdf
aassaadd <aassaadd@foxmail.com>
2016-09-21 16:30:57 +0800

    Update start.sh
    $STEAMGUARD_CODE

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   APP_ID="367540"
 fi
 
-/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login $STEAM_USERNAME $STEAM_PASSWORD $STEAMGUARD_CODE +force_install_dir /starbound/ +app_update $APP_ID +quit
+/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login $STEAM_USERNAME $STEAM_PASSWORD +force_install_dir /starbound/ +app_update $APP_ID +quit
 
 cd /starbound/linux
 
